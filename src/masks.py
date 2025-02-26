f01b95c503d3a509d39eb29d60955b0e5af20c0b
Vitalik <halva1236@gmail.com>
2025-02-26 22:59:04 +0300

    fixes

--- src/masks.py
+++ src/masks.py
@@ -8,7 +8,7 @@ def get_mask_card_number(number_card: int) -> str:
 
 def get_mask_account(account_number: int) -> str:
     """Функцию маскировки номера банковского счета которая принимает счет и возвращает последние 4 цыфры и маскирует 2 перд ними"""
-    text_number2 = str(account_number)  # праброзуем строку в текст
-    mask_numbers2 = "**"  # пишем переменную для маскировски цыфр
+    text_number2 = str(account_number)  # преабразуем строку в текст
+    mask_numbers2 = "**"  # пишем переменную для маскировки цыфр
     resul = f"{mask_numbers2}{text_number2[-5:-1]}"
     return resul
--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,8 @@
-from src import masks
-def mask_account_card(finding_numbers:str)-> str:
+from masks import get_mask_account
+from masks import get_mask_card_number
+
+
+def mask_account_card(finding_numbers: str) -> str:
     string_numbers = ""
     string_letters = ""
     for finding in finding_numbers:
@@ -8,8 +11,19 @@ def mask_account_card(finding_numbers:str)-> str:
         else:
             string_letters += finding
 
-    return string_numbers, string_letters
+    string_lower = string_letters.lower()
+    numbers_int = int(string_numbers)
+    if "счет" not in string_lower:
+        result = get_mask_card_number(numbers_int)
+
+    else:
+        result = get_mask_account(numbers_int)
+
+    return f"{string_letters}{result}"
 
 
+print(mask_account_card('Счет 64686473678894779589'))
 
 
+def get_date():
+    pass
