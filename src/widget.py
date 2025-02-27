108d96a3b512800f360274b7cc1769773cb021c8
Vitalik <halva1236@gmail.com>
2025-02-27 19:53:14 +0300

    fixes

--- src/widget.py
+++ src/widget.py
@@ -1,8 +1,8 @@
-from masks import get_mask_account
+from masks import get_mask_account    # Импортируем необходимые функции.
 from masks import get_mask_card_number
 
 
-def mask_account_card(finding_numbers: str) -> str:
+def mask_account_card(finding_numbers: str) -> str: # Пишем функцию, которая умеет обрабатывать информацию как о картах.
     """Функцию, которая умеет обрабатывать информацию как о картах, так и о счетах, а так же маскирует информацию за счет другой функции."""
     string_numbers = ""
     string_letters = ""
@@ -12,7 +12,7 @@ def mask_account_card(finding_numbers: str) -> str:
         else:
             string_letters += finding
 
-    string_lower = string_letters.lower()
+    string_lower = string_letters.lower() # Пишем условие, которое сортирует на разные функции.
     numbers_int = int(string_numbers)
     if "счет" not in string_lower:
         result = get_mask_card_number(numbers_int)
@@ -23,9 +23,9 @@ def mask_account_card(finding_numbers: str) -> str:
     return f"{string_letters}{result}"
 
 
-print(mask_account_card('Счет 64686473678894779589'))
+print(mask_account_card("Счет 64686473678894779589"))
 
 
-def get_date(string_data: str) -> str:
+def get_date(string_data: str) -> str: # Пишем функцию, которая форматирует код в удобно читаемый вид.
     """Функция, которая принимает на вход строку с датой и создает удобный формат для чтения."""
-    return f"(\"{string_data[8:10]}.{string_data[5:7]}.{string_data[0:4]}\") "
+    return f'("{string_data[8:10]}.{string_data[5:7]}.{string_data[0:4]}") '
