6d009611dc095ea2585e9fafaeff2911fc1c29d1
Vitalik <halva1236@gmail.com>
2025-03-06 18:56:51 +0300

    Fixes-5

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,11 @@
-from typing import Union # Импортируем Union
+from typing import Union  # Импортируем Union
 
 
-def filter_by_state(scroll: list[dict[str, Union[int, str]]], meaning: str = "EXECUTED") -> list[
-    dict[str, Union[int, str]]]: # Создаем функцию, которая отсортирует список
+def filter_by_state(
+    scroll: list[dict[str, Union[int, str]]], meaning: str = "EXECUTED"
+) -> list[dict[str, Union[int, str]]]:  # Создаем функцию, которая отсортирует список
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
- соответствует указанному значению."""
+    соответствует указанному значению."""
     count_dict = []
     for element in scroll:
         if element["state"] == meaning:
@@ -12,11 +13,9 @@ def filter_by_state(scroll: list[dict[str, Union[int, str]]], meaning: str = "EX
     return count_dict
 
 
-
-def sort_by_date(scroll: list[dict[str, Union[int, str]]], direction: bool = True) -> list[
-    dict[str, Union[int, str]]]: # Создаем функцию, которая сортирует по дате
-    """Функция возвращает новый список, отсортированный по дате (date) """
-    sorted_list = sorted(scroll, key=lambda x: x['date'], reverse=direction)
+def sort_by_date(
+    scroll: list[dict[str, Union[int, str]]], direction: bool = True
+) -> list[dict[str, Union[int, str]]]:  # Создаем функцию, которая сортирует по дате
+    """Функция возвращает новый список, отсортированный по дате (date)"""
+    sorted_list = sorted(scroll, key=lambda x: x["date"], reverse=direction)
     return sorted_list
-
-
